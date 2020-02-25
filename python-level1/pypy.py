c2f056823c0205b244271fdc113a720300cff602
pip-peter <peterkir5693@outlook.com>
2020-02-25 19:13:22 +0300

    python code snipsets

---
+++ python-level1/pypy.py
@@ -0,0 +1,60 @@
+"""
+#numbers
+print("hello world")
+
+print(1/2)
+
+print(2**7)
+
+my_dogs = 2
+
+a = 10
+print(a)
+
+a = a + a
+
+print(a)
+
+puppies = 6
+
+weight = 2
+
+total = puppies * weight
+
+print(total)
+
+#strings
+
+print(len("hello"))
+
+#indexing
+
+mystring = "abcdefghij"
+print(mystring[-1])
+
+print(mystring[0:7:2])#start is included  start stop and step 
+print(mystring[::3])
+
+"""
+"""
+mystring = "hello world"
+
+print(mystring.split())
+"""
+
+username = "sammy"
+
+color = "blue"
+
+print("the {} favourite is {}".format(username,color))
+
+print(f"The {username} chose {color}" )
+
+#lists
+#they ordered sequences that can hold a number of objects
+
+myList = [1,2,3]
+print(len(myList))
+
+
+
